968c7fa53e8b676b7fff6e7d9310b0165739c1f5
aloneinabyss <thiago-assi@outlook.com>
2022-02-07 21:01:41 -0300

    reading operations file manually

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            File operacoes = new File("D:\\Arquivos\\Projetos\\Projetos\\Projetos IntelliJ\\calculadora-saldo-bancario\\src\\data\\operacoes.csv");
+            Scanner scanner = new Scanner(operacoes);
+
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro: " + e);
+        }
     }
 }
